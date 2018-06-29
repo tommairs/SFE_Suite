b9b0cce558bdfb9af53f4deaa57a6b53a3d8e3a8
Tom Mairs <tom.mairs@sparkpost.com>
2018-06-29 11:29:02 -0600

    update for https redirect

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+// make sure we start with a secure connection.
+if ($_SERVER['SERVER_PORT'] != "443"){
+   header("Location: https://suite.trymsys.net");
+   die();
+}
+
+
+?>
+
+<html>
+
+ <head>
+  <title>
+    Simple Front End Suite
+  </title>
+ </head>
+ <body>
+  <h1>
+    Simple Front End Suite
+  </h1>
+<p>
+<b>Select a function:</b> </br>
+<a href="/mosaico/">Template Editor</a><br>
+<a href="/archiver/search.html">Search the message archive</a><br>
+<a href="/infilter/inbox.html">Manage the Inbox</a><br>
+<a href="/workflow/mgr.html">Manage Workflows</a><br>
+<a href="/vib/mgr.html">Manage View-In-Browser data</a><br>
+<a href="/admin/">Site Administration</a><br>
+
+</p>
+ </body>
+</html>
